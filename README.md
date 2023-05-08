563b593958845735c949dcc681c8be3c01ac6880
Corné Dorrestijn <contact@corne.info>
2023-05-08 20:42:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+#  AirTS
+
+Enable AirPlay on the NTS.live website using a Safari Extension 
+
+![Screenshot](./screenshot.jpeg)
+
+## Getting started
+
+This project is not published to the App Store at the moment. To run this project:
+
+1. Clone and open the project in XCode
+2. Press the Run button to start the project.
+3. Enable the extension in Safari.
--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,7 +1,82 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
+async function showPlaybackTargetPickerForActiveAudio() {
+    // Loop over all audio elements, and find the playing one. At this
+    // time there are two options:
+    //   1. The audio element used for live streams: `#nts-player-audio`
+    //   2. Another audio element that is used for on-demand streams.
+    const audios = document.querySelectorAll('audio');
+    for (const audio of audios) {
+        if (audio.paused) {
+            continue;
+        }
+        try {
+            audio.webkitShowPlaybackTargetPicker();
+        } catch {
+            // if it fails, it fails ¯\_(ツ)_/¯
+        }
+        return
+    }
+}
+
+const airplayIcon = document.createElement('img')
+airplayIcon.src = browser.runtime.getURL("images/airplay.svg");
+
+// Button that is shown on desktop
+const airTSButton = document.createElement('button');
+airTSButton.classList = 'live-header__airplay';
+airTSButton.appendChild(airplayIcon);
+
+airTSButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    showPlaybackTargetPickerForActiveAudio();
 });
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+// Button that is shown on mobile
+const mobileAirTSButton = document.createElement('button');
+mobileAirTSButton.classList = 'live-header__footer__button live-header__airplay';
+mobileAirTSButton.appendChild(airplayIcon.cloneNode());
+
+mobileAirTSButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    showPlaybackTargetPickerForActiveAudio();
 });
+
+// TODO: There might be a faster solution. (MutationObserver perhaps?)
+window.addEventListener('load', () => {
+    // When AirPlaying the live player, the audio often switches back to
+    // the host device. To fight this the pause event is prevented. This
+    // event is randomly triggered when focus is lost/regained, or when
+    // the user navigates the app.
+    // This will not change the behaviour of the app, if the stop button
+    // is clicked, the source is set, and the emptied event is triggered.
+    const live = document.querySelector('#nts-player-audio')
+    live.addEventListener('pause', (e) => {
+        e.preventDefault();
+        console.log('AirTS - Pause prevented', e)
+    })
+    live.addEventListener('play', (e) => {
+        e.preventDefault();
+        console.log('AirTS - Play triggered', e)
+        // Re-triggering play. This also seems to prevent some issues. Need
+        // to investigate further.
+        live.play();
+    })
+    live.addEventListener('stop', (e) => {
+        e.preventDefault();
+        console.log('AirTS - Stop prevented', e)
+    })
+    live.addEventListener('emptied', e => console.log('AirTS - emptied', e))
+    
+    // Add a AirPlay button to the live header on desktop...
+    const liveHeader = document.querySelector('.live-header__footer:not(.live-header__footer--mobile)');
+    if (liveHeader) {
+        const volumeButton = liveHeader.querySelector('.volume-control');
+        liveHeader.insertBefore(airTSButton, volumeButton);
+    }
+    
+    // ... and on mobile
+    const liveHeaderMobile = document.querySelector('.live-header__footer--mobile')
+    if (liveHeaderMobile) {
+        liveHeaderMobile.insertBefore(mobileAirTSButton, liveHeaderMobile.childNodes[0]);
+    }
+})
+
